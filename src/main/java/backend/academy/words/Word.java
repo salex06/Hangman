47ae86d3f7ad224c82ace258b9e21262eaab52f8
Alexey Sazonov <salex.9206@mail.ru>
2024-09-14 19:46:44 +0300

    feat: create a "Word" class
    Create a "Word" class because using Word class objects to represent a word is preferable for the program architecture than using simple strings. The object stores all the necessary fields to get information about the word

---
+++ src/main/java/backend/academy/words/Word.java
@@ -0,0 +1,20 @@
+package backend.academy.words;
+
+import backend.academy.words.enums.Category;
+import backend.academy.words.enums.Level;
+import lombok.Getter;
+
+@Getter
+public class Word {
+    private final String value;
+    private final String hint;
+    private final Category category;
+    private final Level level;
+
+    public Word(String value, String hint, Category category, Level level) {
+        this.value = value;
+        this.hint = hint;
+        this.category = category;
+        this.level = level;
+    }
+}
