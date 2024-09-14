3d0bfd050765008800c28c46f0695da8cfa11395
Alexey Sazonov <salex.9206@mail.ru>
2024-09-14 20:54:23 +0300

    feat(Word): add equals and hashCode methods

--- src/main/java/backend/academy/words/Word.java
+++ src/main/java/backend/academy/words/Word.java
@@ -2,6 +2,7 @@ package backend.academy.words;
 
 import backend.academy.words.enums.Category;
 import backend.academy.words.enums.Level;
+import java.util.Objects;
 import lombok.Getter;
 
 @Getter
@@ -20,5 +21,22 @@ public class Word {
 
     @Override public String toString() {
         return value;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Word word = (Word) o;
+        return Objects.equals(value, word.value) && Objects.equals(hint, word.hint) && category == word.category
+            && level == word.level;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, hint, category, level);
     }
 }
