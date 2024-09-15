33e621dc3645b7cb83b251de6cf033107b411a26
Alexey Sazonov <salex.9206@mail.ru>
2024-09-15 16:25:08 +0300

    refactor: replace the word class with a record

--- src/main/java/backend/academy/words/Word.java
+++ src/main/java/backend/academy/words/Word.java
@@ -3,21 +3,8 @@ package backend.academy.words;
 import backend.academy.words.enums.Category;
 import backend.academy.words.enums.Level;
 import java.util.Objects;
-import lombok.Getter;
 
-@Getter
-public class Word {
-    private final String value;
-    private final String hint;
-    private final Category category;
-    private final Level level;
-
-    public Word(String value, String hint, Category category, Level level) {
-        this.value = value;
-        this.hint = hint;
-        this.category = category;
-        this.level = level;
-    }
+public record Word(String value, String hint, Category category, Level level) {
 
     @Override public String toString() {
         return value;
