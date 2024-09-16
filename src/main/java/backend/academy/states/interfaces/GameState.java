843d5c9ec1c138ab57cbf40ced77d47905674cba
Alexey Sazonov <salex.9206@mail.ru>
2024-09-16 12:08:10 +0300

    docs: add JavaDoc to a GameState interface

--- src/main/java/backend/academy/states/interfaces/GameState.java
+++ src/main/java/backend/academy/states/interfaces/GameState.java
@@ -3,10 +3,29 @@ package backend.academy.states.interfaces;
 import backend.academy.game.implementations.GameSession;
 import java.io.IOException;
 
+/**
+ * The interface of the "State" pattern to manage game states
+ */
 public interface GameState {
+    /**
+     * Change the current state to the next state
+     *
+     * @param gameSession the current session of the game
+     */
     void nextState(GameSession gameSession);
 
+    /**
+     * Handle the current state of the game (configuring the game, playing the game, etc.)
+     *
+     * @param gameSession the current session of the game
+     * @throws IOException if an input/output error has occurred
+     */
     void processState(GameSession gameSession) throws IOException;
 
+    /**
+     * Check the game is finished
+     *
+     * @return boolean type value - the result of the check
+     */
     boolean gameIsFinished();
 }
