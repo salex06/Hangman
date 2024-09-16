b5b7daf6cbf5bc7aef35da910466cf34b9e8d26d
Alexey Sazonov <salex.9206@mail.ru>
2024-09-16 13:42:37 +0300

    docs: add JavaDoc to a PlayingGameState class

--- src/main/java/backend/academy/states/implementations/PlayingGameState.java
+++ src/main/java/backend/academy/states/implementations/PlayingGameState.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The PlayingGameState class implements GameState interface and
+ * represents the state of the main part of the game.
+ */
 @Getter
 @Setter
 public class PlayingGameState implements GameState {
@@ -16,16 +20,32 @@ public class PlayingGameState implements GameState {
     private StringBuilder attemptStringBuilder;
     private boolean gameResult;
 
+    /**
+     * Defines the currentAttemptNumber and attemptStringBuilder fields
+     *
+     * @param stringLength the length of the answer
+     */
     public PlayingGameState(int stringLength) {
         currentAttemptNumber = 0;
         attemptStringBuilder = new StringBuilder().append("-".repeat(Math.max(0, stringLength)));
     }
 
+    /**
+     * Sets the game state as ResultOfTheGameState
+     *
+     * @param gameSession the current session of the game
+     */
     @Override
     public void nextState(GameSession gameSession) {
         gameSession.gameState(new ResultOfTheGameState(gameResult));
     }
 
+    /**
+     * Handles the main game process
+     *
+     * @param gameSession the current session of the game
+     * @throws IOException if an input/output error has occurred
+     */
     @Override
     public void processState(GameSession gameSession) throws IOException {
         ConsoleInteractor consoleInteractor = gameSession.consoleInteractor();
@@ -62,6 +82,14 @@ public class PlayingGameState implements GameState {
         consoleInteractor.clearConsole();
     }
 
+    /**
+     * Controls the current state of the game according to the current attempt
+     *
+     * @param answer           the hidden word
+     * @param numberOfAttempts the current number of attempts
+     * @param currentLetter    the entered character
+     * @return true if the game is over; false if the game hasn't been over yet
+     */
     public boolean manageStateData(String answer, int numberOfAttempts, String currentLetter) {
         if (currentLetter.equals(String.valueOf('-'))) {
             needHint = true;
@@ -81,6 +109,13 @@ public class PlayingGameState implements GameState {
         return false;
     }
 
+    /**
+     * Manages the StringBuilder - the state of the hidden word - according to the current attempt
+     *
+     * @param answer        the hidden word
+     * @param currentLetter the entered character
+     * @return true if the currentLetter is found in the answer, else - false
+     */
     public boolean updateAttemptStringBuilder(String answer, char currentLetter) {
         boolean wasUpdated = false;
         for (int i = 0; i < answer.length(); i++) {
@@ -92,6 +127,12 @@ public class PlayingGameState implements GameState {
         return wasUpdated;
     }
 
+    /**
+     * Compares the answer with the current word
+     *
+     * @param answer the hidden word
+     * @return true if answer is equal to the hidden word, else - false
+     */
     public boolean compareAnswerToCurrent(String answer) {
         String compareToAnswer = attemptStringBuilder.toString();
         boolean successfulCompare = true;
@@ -104,6 +145,11 @@ public class PlayingGameState implements GameState {
         return successfulCompare;
     }
 
+    /**
+     * Check the game is finished (it's always false, because the game has not finished yet)
+     *
+     * @return boolean type variable "gameIsFinished"
+     */
     @Override
     public boolean gameIsFinished() {
         return gameIsFinished;
