d7770d9ace6a59a973056a9e6905776c92cb05ee
Alexey Sazonov <salex.9206@mail.ru>
2024-09-16 12:42:26 +0300

    docs: add JavaDoc to a ResultOfTheGameState class

--- src/main/java/backend/academy/states/implementations/ResultOfTheGameState.java
+++ src/main/java/backend/academy/states/implementations/ResultOfTheGameState.java
@@ -5,18 +5,38 @@ import backend.academy.states.interfaces.GameState;
 import backend.academy.util.ConsoleInteractor;
 import java.io.IOException;
 
+/**
+ * ResultOfTheGameState class implements GameState interface and handles the state of the game if
+ * it finished (the user won or lost)
+ */
 public class ResultOfTheGameState implements GameState {
     boolean gameIsFinished = false;
     private final boolean gameResult;
 
+    /**
+     * Class constructor with one boolean type argument
+     *
+     * @param gameResult result of the game (the user won or lost)
+     */
     public ResultOfTheGameState(boolean gameResult) {
         this.gameResult = gameResult;
     }
 
+    /**
+     * Does nothing because the next state is undefined (the game is over)
+     *
+     * @param gameSession the current session of the game
+     */
     @Override
     public void nextState(GameSession gameSession) {
     }
 
+    /**
+     * Process game results
+     *
+     * @param gameSession the current session of the game
+     * @throws IOException if an input/output error has occurred
+     */
     @Override
     public void processState(GameSession gameSession) throws IOException {
         ConsoleInteractor consoleInteractor = gameSession.consoleInteractor();
@@ -30,6 +50,11 @@ public class ResultOfTheGameState implements GameState {
         gameIsFinished = true;
     }
 
+    /**
+     * Check the game is finished
+     *
+     * @return boolean type variable "gameIsFinished"
+     */
     @Override
     public boolean gameIsFinished() {
         return gameIsFinished;
