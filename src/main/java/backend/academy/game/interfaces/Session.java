0d86904b7005a515838e65ebb74b40f3218c5ddd
Alexey Sazonov <salex.9206@mail.ru>
2024-09-16 14:39:05 +0300

    docs: add JavaDoc to a Session interface

--- src/main/java/backend/academy/game/interfaces/Session.java
+++ src/main/java/backend/academy/game/interfaces/Session.java
@@ -2,6 +2,14 @@ package backend.academy.game.interfaces;
 
 import java.io.IOException;
 
+/**
+ * A functional interface that has a single method for starting a session
+ */
 public interface Session {
+    /**
+     * Starts the session
+     *
+     * @throws IOException if an input/output error has occurred
+     */
     void start() throws IOException;
 }
