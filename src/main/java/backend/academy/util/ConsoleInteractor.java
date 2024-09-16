e8615926db1185a263cf43954c09d11e51a4cf80
Alexey Sazonov <salex.9206@mail.ru>
2024-09-16 07:22:48 +0300

    docs: add JavaDoc to a ConsoleInteractor class

--- src/main/java/backend/academy/util/ConsoleInteractor.java
+++ src/main/java/backend/academy/util/ConsoleInteractor.java
@@ -8,6 +8,12 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import lombok.Getter;
 
+/**
+ * ConsoleInteractor class allows to interact with the console
+ * (reading/writing data, flushing the buffer, clearing the console).
+ * It is based on the BufferedReader and BufferedWriter classes using
+ * System.in and System.out.
+ */
 @Getter
 public class ConsoleInteractor {
     private final BufferedReader bufferedReader;
@@ -18,18 +24,41 @@ public class ConsoleInteractor {
         bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
     }
 
+    /**
+     * Writes data to a BufferedWriter
+     *
+     * @param message data for writing
+     * @throws IOException if an output error has occurred
+     */
     public void writeMessage(String message) throws IOException {
         bufferedWriter.write(message);
     }
 
+    /**
+     * Reads data from a bufferedReader
+     *
+     * @return user input
+     * @throws IOException if an input error has occurred
+     */
     public String readMessage() throws IOException {
         return bufferedReader.readLine();
     }
 
+    /**
+     * Flushes the bufferedWriter for displaying messages to the console
+     *
+     * @throws IOException if a buffer flushing error has occurred
+     */
     public void flushBuffer() throws IOException {
         bufferedWriter.flush();
     }
 
+    /**
+     * Clears the console of previous messages.
+     * It only works in consoles with ASCII code support
+     *
+     * @throws IOException if an output error has occurred
+     */
     public void clearConsole() throws IOException {
         writeMessage("\033[H\033[2J");
         flushBuffer();
