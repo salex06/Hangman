5a94cf6c7f0f0cfdd2adf664f438167e0d7bdc6a
Alexey Sazonov <salex.9206@mail.ru>
2024-09-19 18:09:02 +0300

    docs: add JavaDoc to IOHandler

--- src/main/java/backend/academy/util/IOHandler.java
+++ src/main/java/backend/academy/util/IOHandler.java
@@ -1,7 +1,18 @@
 package backend.academy.util;
 
+/**
+ * IOHandler interface provides read and write operations
+ */
 public interface IOHandler {
+    /**
+     * Write a string to the output stream
+     * @param message message for output
+     */
     void writeMessage(String message);
 
+    /**
+     * Read a string from the input stream
+     * @return message from the input stream
+     */
     String readMessage();
 }
