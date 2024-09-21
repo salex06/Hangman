35befc65540d471e9c9451c3a6f8bb6e235d0cfd
Alexey Sazonov <salex.9206@mail.ru>
2024-09-21 21:04:48 +0300

    refactor: add missing JavaDocs to the IStrategyManager

--- src/main/java/backend/academy/strategy/IStrategyManager.java
+++ src/main/java/backend/academy/strategy/IStrategyManager.java
@@ -2,6 +2,17 @@ package backend.academy.strategy;
 
 import backend.academy.game.implementations.GameSession;
 
+/**
+ * The IStrategyManager interface provides the operation
+ * which allows get the current strategy for the game according to
+ * the information in the model of the game
+ */
 public interface IStrategyManager {
+    /**
+     * The method of managing game strategies
+     *
+     * @param gameSession the model of the game which contains all the necessary info
+     * @return GameStrategy type object which defines the behaviour of the current game state
+     */
     GameStrategy manageStrategy(GameSession gameSession);
 }
