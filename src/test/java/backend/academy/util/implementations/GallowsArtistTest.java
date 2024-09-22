f49f2239a13944aa579b3d9cb8974fcd4c4e106c
Alexey Sazonov <salex.9206@mail.ru>
2024-09-22 10:48:16 +0300

    refactor: move GallowsArtistTest to the util package

--- src/test/java/backend/academy/util/implementations/GallowsArtistTest.java
+++ src/test/java/backend/academy/util/implementations/GallowsArtistTest.java
@@ -1,6 +1,5 @@
-package backend.academy.game;
+package backend.academy.util.implementations;
 
-import backend.academy.util.implementations.GallowsArtist;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
